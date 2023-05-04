5f39b1b8a599f61a12bc324840ae4f6ec42e2a03
(*-*) Mike (*-*) <131814408+Star-Dreamer-928@users.noreply.github.com>
2023-05-04 15:34:27 -0500

    Create tbl.py
    Code for math table. May just call it a math list soon)

---
+++ tbl.py
@@ -0,0 +1,13 @@
+# Math table (Still needs work)
+
+start_range = int(input("Enter beggining number: "))
+
+end_range = int(input("Enter ending number: "))
+
+for i in range(start_range, end_range + 1):
+  for j in range(1, 11):
+    print(i*j)
+  print() 
+  
+  # Still not a table
+  
