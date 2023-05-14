fbbcdafd3094e44c9f16048c02d41ca510123e6c
(*-*) Mike (*-*) <131814408+Star-Dreamer-928@users.noreply.github.com>
2023-05-13 22:02:28 -0500

    Update divd.py

--- divd.py
+++ divd.py
@@ -1,8 +1,36 @@
-# Division function
+import tkinter as tk
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+root = tk.Tk()
+root.title("Division Window")
 
-qoutient = num1 / num2
+header_label = tk.Label(root, text="Division")
+header_label.pack
 
-print(num1, "+", num2, "=", qoutient)
+bg = PhotoImage(Background_Images="red_bg.jpg")
+
+def div_numbers():
+    num1 = int(input(entry1.get())
+    num2 = int(input(entry2.get())
+    result = num1 / num2
+    
+qot_label.config(text="qoutient:" + str(result))
+
+root = tk.Tk()
+root.title("Divide")
+
+entry1 = tk.Entry(root)
+entry1.pack()
+
+entry2 = tk.Entry(root)
+entry2.pack()
+
+div_button = tk.Button(root, text="Division", command=div_numbers)
+div_button.pack
+
+qot_label = tk.Label(root, text="Divide: ")
+qot_label.pack()
+
+exit = tk.Button(root, text="Exit", command=root.destroy)
+exit.pack(pady=20)
+
+root.mainloop()
