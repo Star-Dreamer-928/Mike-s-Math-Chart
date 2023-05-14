d183d714e49414aec7c5ef42124e63c99ac20b50
(*-*) Mike (*-*) <131814408+Star-Dreamer-928@users.noreply.github.com>
2023-05-13 21:59:57 -0500

    Update mult.py
    Code for Multiplication window

--- mult.py
+++ mult.py
@@ -1,8 +1,36 @@
-# Multiplictation function
+import tkinter as tk
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+root = tk.Tk()
+root.title("Division Window")
 
-product = num1 * num2
+header_label = tk.Label(root, text="Multiplication")
+header_label.pack
 
-print(num1, "*", num2, "=", product)
+bg = PhotoImage(Background_Images="blue2_bg.jpg")
+
+def mul_numbers():
+    num1 = int(input(entry1.get())
+    num2 = int(input(entry2.get())
+    result = num1 * num2
+    
+pro_label.config(text="product:" + str(result))
+
+root = tk.Tk()
+root.title("Multiplication")
+
+entry1 = tk.Entry(root)
+entry1.pack()
+
+entry2 = tk.Entry(root)
+entry2.pack()
+
+mul_button = tk.Button(root, text="Multiplication", command=mul_numbers)
+mul_button.pack
+
+pro_label = tk.Label(root, text="Multiply: ")
+pro_label.pack()
+
+exit = tk.Button(root, text="Exit", command=root.destroy)
+exit.pack(pady=20)
+
+root.mainloop()
