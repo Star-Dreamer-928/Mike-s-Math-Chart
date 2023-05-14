b7890edc3c3437c8af83b70093bc415a823b2c5e
(*-*) Mike (*-*) <131814408+Star-Dreamer-928@users.noreply.github.com>
2023-05-13 21:57:14 -0500

    Update sub.py
    Code for Subtraction Window

--- sub.py
+++ sub.py
@@ -1,8 +1,36 @@
-# Subtraction function
+import tkinter as tk
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+root = tk.Tk()
+root.title("Subtraction Window")
 
-remainder = num1 - num2
+header_label = tk.Label(root, text="Subtraction")
+header_label.pack
 
-print(num1, "-", num2, "=", remainder)
+bg = PhotoImage(Background_Images="yellow_bg.jpg")
+
+def sub_numbers():
+    num1 = int(input(entry1.get())
+    num2 = int(input(entry2.get())
+    result = num1 - num2
+    
+rem_label.config(text="remainder:" + str(result))
+
+root = tk.Tk()
+root.title("Subtraction")
+
+entry1 = tk.Entry(root)
+entry1.pack()
+
+entry2 = tk.Entry(root)
+entry2.pack()
+
+sub_button = tk.Button(root, text="Subtract", command=add_numbers)
+add_button.pack
+
+rem_label = tk.Label(root, text="Subtract: ")
+rem_label.pack()
+
+exit = tk.Button(root, text="Exit", command=root.destroy)
+exit.pack(pady=20)
+
+root.mainloop()
