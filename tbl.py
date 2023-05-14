bdb1449a6d7197efd918db57446cb82a1dc6a1e1
(*-*) Mike (*-*) <131814408+Star-Dreamer-928@users.noreply.github.com>
2023-05-13 22:03:38 -0500

    Update tbl.py
    Code for Multiplication table

--- tbl.py
+++ tbl.py
@@ -1,13 +1,28 @@
-# Math table (Still needs work)
+import tkinter as tk
 
-start_range = int(input("Enter beggining number: "))
+root = tk.Tk()
+root.title("Table Window")
 
-end_range = int(input("Enter ending number: "))
+header_label = tk.Label(root, text="Custom Math Table")
+header_label.pack
 
-for i in range(start_range, end_range + 1):
-  for j in range(1, 11):
-    print(i*j)
-  print() 
-  
-  # Still not a table
-  
+bg = PhotoImage(Background_Images="blue_bg.jpg")
+
+def tab_list():
+    start_range = int(start_entry.get())
+    end_range = int(end_entry.get())
+
+    column_width = 6
+    
+    table_text.telete("1.0", tk.END)
+    
+    for i in range(start_range, end_range + 1):
+        row_text = ""
+        for j in range(1, end_range + 1):
+            row_text += f"{i} x {j}" = {i*j: {column_width}}"
+            table_text.insert(tk.End, row_text + "\n")
+
+exit = tk.Button(root, text="Exit", command=root.destroy)
+exit.pack(pady=20)
+               
+root.mainloop()
