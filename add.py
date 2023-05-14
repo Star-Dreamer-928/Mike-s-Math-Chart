5511aea3b623a69fd5dd9167f5d4c266d2507d29
(*-*) Mike (*-*) <131814408+Star-Dreamer-928@users.noreply.github.com>
2023-05-13 21:55:23 -0500

    Update add.py
    Code for Addition window.

--- add.py
+++ add.py
@@ -1,8 +1,36 @@
-# Addition functions
+import tkinter as tk
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+root = tk.Tk()
+root.title("Addition Window")
 
-summ = num1 + num2
+header_label = tk.Label(root, text="Addition")
+header_label.pack
 
-print(num1, "+", num2, "=", summ)
+bg = PhotoImage(Background_Images="green_bg.jpg")
+
+def add_numbers():
+    num1 = int(input(entry1.get())
+    num2 = int(input(entry2.get())
+    result = num1 + num2
+    
+sum_label.config(text="Sum:" + str(result))
+
+root = tk.Tk()
+root.title("Addition")
+
+entry1 = tk.Entry(root)
+entry1.pack()
+
+entry2 = tk.Entry(root)
+entry2.pack()
+
+add_button = tk.Button(root, text="Add", command=add_numbers)
+add_button.pack
+
+sum_label = tk.Label(root, text="Sum: ")
+sum_label.pack()
+
+exit = tk.Button(root, text="Exit", command=root.destroy)
+exit.pack(pady=20)
+               
+root.mainloop()
